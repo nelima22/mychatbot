b8eb99312963b1cbaffc500acbadfdde99fae531
Esther Nelima <122277757+nelima22@users.noreply.github.com>
2024-09-09 13:37:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
+
 import streamlit as st
 import os
 from beyondllm import source, retrieve, embeddings, llms, generator
